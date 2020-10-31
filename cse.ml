4c6d6229e03192fbd3b31f70398ab490a73d883a
Joe Hattori <joe2ninja21@gmail.com>
2020-10-31 09:02:45 +0900

    improve cse

--- cse.ml
+++ cse.ml
@@ -1,12 +1,11 @@
 open KNormal
 
 let is_equal = function
-  | Add(x1, y1), Add(x2, y2) -> x1 = x2 && y1 = y2
-  | Sub(x1, y1), Sub(x2, y2) -> x1 = x2 && y1 = y2
-  | FAdd(x1, y1), FAdd(x2, y2) -> x1 = x2 && y1 = y2
-  | FSub(x1, y1), FSub(x2, y2) -> x1 = x2 && y1 = y2
-  | FMul(x1, y1), FMul(x2, y2) -> x1 = x2 && y1 = y2
-  | FDiv(x1, y1), FDiv(x2, y2) -> x1 = x2 && y1 = y2
+  | Add(x1, y1), Add(x2, y2) | Sub(x1, y1), Sub(x2, y2)
+  | FAdd(x1, y1), FAdd(x2, y2) | FSub(x1, y1), FSub(x2, y2)
+  | FMul(x1, y1), FMul(x2, y2) | FDiv(x1, y1), FDiv(x2, y2)
+    -> x1 = x2 && y1 = y2
+  | Neg(x1), Neg(x2) | FNeg(x1), FNeg(x2) -> x1 = x2
   | _ -> false
 
 let rec search env key =
@@ -19,6 +18,7 @@ let rec search env key =
 let rec g env ex = match ex with
   | Add(x, y) | Sub(x, y) | FAdd(x, y) | FSub(x, y) | FMul(x, y) | FDiv(x, y) ->
     search env ex
+  | Neg(x) | FNeg(x) -> search env ex
   | IfEq(x, y, e1, e2) -> IfEq(x, y, g env (search env e1), g env (search env e2))
   | IfLE(x, y, e1, e2) -> IfLE(x, y, g env (search env e1), g env (search env e2))
   | Let((x, t), e1, e2) ->
