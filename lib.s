a2aa123704b575627f52aaa32b23e053f8219291
Joe Hattori <joe2ninja21@gmail.com>
2020-11-27 16:53:40 +0900

    Implement floor.

--- lib.s
+++ lib.s
@@ -1,4 +1,6 @@
 	.data
+l.one:	# 1.000000
+	.word	0x3f800000
 l.ftoi_cmp: # 8388608.0
 	.word	0x4b000000
 	.text
@@ -106,5 +108,24 @@ ftoi_sum_cont:
 
 	.globl min_caml_floor
 min_caml_floor:
-	fcvt.w.s	a0, fa0
+	la	t6, l.ftoi_cmp
+	flw	ft0, 0(t6)
+	fsgnjx.s	ft1, fa0, fa0
+	fsgnj.s	ft2, fa0, fa0
+	flt.s	t3, ft1, ft0
+	beq	t3, zero, floor_ret
+	sw	a0, 4(s0)
+	sw	ra, 8(s0)
+	addi	s0, s0, 12
+	jal	min_caml_int_of_float
+	jal	min_caml_float_of_int
+	addi	s0, s0, -12
+	lw	ra, 8(s0)
+	lw	a0, 4(s0)
+	flt.s	t3, ft2, fa0
+	beq	t3, zero, floor_ret
+	la	t6, l.one
+	flw	ft2, 0(t6)
+	fsub.s	fa0, fa0, ft2
+floor_ret:
 	ret
